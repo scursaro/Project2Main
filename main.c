a7ce00bb194f4f63372e25f518b5e10cfd4f7f08
Sebastian Cursaro <scursaro@cpp.edu>
2022-11-29 14:01:22 -0800

    added all of the necessary varaibles

--- main.c
+++ main.c
@@ -5,11 +5,47 @@
 
 
 //also need to include the basics, and include the other files
+#include <stdlib.h>
+#include <stdio.h>
 
 int main()
 {
     //we will need a lot of input from the user
     //a lot of varaibles need to be created as well
+
+    int days;
+    double arrivalTime;
+    double airfareFee;
+    double carRentalFee;
+    double privateCarFee;
+    double vechileExpense;
+    double privateCarMilage = .38;
+    double parkingFee;
+    double taxiFee;
+    double registrationFee;
+    double spentTotal;//Overall total cost
+    double allowedTotal;
+    double companyTotal;
+    double mealTotal;
+    double total;
+    double allowedParking;
+    double allowedTaxiFee;
+    double hotelFee;
+    double nightlyRate;
+    double allowedHotelFee;
+    double hotelFeeTotal;
+    double allowedBreaksfastFee;
+    double allowedLunchFee;
+    double allowedDinnerFee;
+    double breaksfastFee;
+    double lunchFee;
+    double dinnerFee;
+    double allowedMealTotal;
+    char employeename[15]; //15 since I do not think a name of 15 characters exists
+    int d;
+    double parkingSpent;
+    double taxiSpent;
+    double spentMealTotal;
 
     //functoin calls to the unfinished functions
 
