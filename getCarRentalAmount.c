3746f2eeb33efa7e41bf8f9718e21d329fffc4fd
mayela101 <113002207+mayela101@users.noreply.github.com>
2022-11-30 12:12:43 -0800

    Car Rental function
    function that is used for car rental expenses

---
+++ getCarRentalAmount.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+//func that is used for car rental expenses
+double getCarRentalAmount()
+{
+	printf("What was the total cost of any car rentals?\n");
+	scanf("%lf", &carRentalFee);
+
+	while(carRentalFee < 0)
+	{
+		printf("Error: Please enter a number greater than 0. Try again!\n");
+		scanf("%lf", &carRentalFee);
+	}
+		
+	return carRentalFee;
+}
