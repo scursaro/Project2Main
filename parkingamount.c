0f7628516421a32d107ba24bc70c6ec84f069f7b
adroman1cpp <112600733+adroman1cpp@users.noreply.github.com>
2022-11-29 20:39:05 -0800

    Add files via upload
    added comments

--- parkingamount.c
+++ parkingamount.c
@@ -2,15 +2,20 @@ include <stdio.h>
 
 double parkingAmount(int days){
     
+    //create variable
     double parkingCharge;
 
+    //ask the user for the amount of money spent on parking per day.
     printf("How much money was spent on parking per day?");
+    //get the input for the user.
     scanf("%lf", &parkingCharge);
 
+    //loop that ensures the user has a vaid input for days.
     while (parkingFee < 0){
         printf("Invalid Input. Enter a non-negative amount:");
         scanf("%lf", &parkingFee);
     }
 
+    //return the amount of money spent per day times the amount of days spent.
     return parkingCharge * days;
 }
