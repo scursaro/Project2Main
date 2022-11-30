5ecb967e16c6171699ef5bcdfde22f028c3f6fe9
mayela101 <113002207+mayela101@users.noreply.github.com>
2022-11-30 12:13:38 -0800

    Private Vehicle function
    function for private vehicle expenses

---
+++ getPrivateVehicleExpense.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+//func for private vehicle expenses 
+double getPrivateVehicleExpense()
+{
+	const double RATE = 0.38;
+	double miles;
+
+	printf("How many miles were driven in a private vehicle?\n");
+	scanf("%lf", &miles);
+	
+	while(miles < 0)
+	{
+		printf("Error: Please enter a number greater than 0. Try again!\n");
+		scanf("%lf", &miles);
+	}
+	
+	privateCarFee = miles * RATE;
+	return privateCarFee;
+}
