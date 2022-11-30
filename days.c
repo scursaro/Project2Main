6594531ca21d8cad41bd6e764af7ce4b971dc60a
adroman1cpp <112600733+adroman1cpp@users.noreply.github.com>
2022-11-29 19:24:11 -0800

    Add files via upload
    added comments

--- days.c
+++ days.c
@@ -1,11 +1,16 @@
 include <stdio.h>
 
 int numberOfDays(){
-
+    
+    //create variable for the days.
     int days;
+
+    //ask the user for the amount of days.
     printf("How many days did the trip last?");
+    //get the input for the user.
     scanf("%lf", &days);
 
+    //loop that ensures the user has a vaid input for days.
     while(days < 1){
         printf("How many days did the trip last?");
         scanf("%lf", &days);
