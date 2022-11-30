c1ef35c73675f48a9513d9eef55f038b96c9b6a7
Joseph-Bui <112789622+Joseph-Bui@users.noreply.github.com>
2022-11-29 20:35:08 -0800

    Initial commit with comments

---
+++ MealAmount.c
@@ -0,0 +1,10 @@
+//this is the funcion that will obtain the amount for each meal
+
+//funcion will need to obtain user input for the price of specifc meals such as breakfast, lunch, and dinner
+
+//function also needs local vairables to store the user input
+
+//prices of meals will be asked based on the number of days spent on the trip and the time of departure
+
+//after obtaining user input for meal prices, calculate the meal total
+
