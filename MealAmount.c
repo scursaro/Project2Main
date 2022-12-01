18dfd5fb96e534614fdb54ddc45b1684e2675d42
Joseph-Bui <112789622+Joseph-Bui@users.noreply.github.com>
2022-11-30 17:30:16 -0800

    Updated #include issue

--- MealAmount.c
+++ MealAmount.c
@@ -1,4 +1,6 @@
 //this is the funcion that will obtain the amount for each meal
+#include <stdio.h>
+
 void getMealAmount(int numberDays)
 {
     //create local variables to store the user input
@@ -12,23 +14,19 @@ void getMealAmount(int numberDays)
     for(days = 1; days <= numDays; days++)
     {
         printf("\nDay: %d\n", days);
-        
+
         if(days < 2 && departureTime > 0.00 && departureTime <= 7.00)
         {
-            //ask for the price of breakfast
             printf("Enter the amount for breakfast: ");
             scanf("%lf", &breakfastFee);
-            
-            //ask for the price of lunch
+
             printf("Enter the amount for lunch: ");
             scanf("%lf", &lunchFee);
 
-            //ask for the price of dinner
             printf("Enter the amount for dinner: ");
             scanf("%lf", &dinnerFee);
         }
-        
-        //only ask for lunch and dinner if departure time is past 7
+
         if(days < 2 && departureTime > 7.01 && departureTime <= 12.00)
         {
             printf("Enter the amount for lunch: ");
@@ -37,15 +35,13 @@ void getMealAmount(int numberDays)
             printf("Enter the amount for dinner: ");
             scanf("%lf", &dinnerFee);
         }
- 
-        //only ask for lunch if departure time is past 12pm
+
         if(days < 2 && departureTime > 12.01 && departureTime <= 18.00)
         {
             printf("Enter the amount for dinner: ");
             scanf("%lf", &dinnerFee);
         }
-        
-        //ask for breakfast, lunch, and dinner fees
+
         if(days > 1 && days < numDays)
         {
             printf("Enter the amount for breakfast: ");
@@ -60,13 +56,11 @@ void getMealAmount(int numberDays)
 
         if(days == numDays && arrivalTime > 8.00 && arrivalTime <= 13.00)
         {
-            //ask for breakfast amount
             printf("Enter the amount for breakfast: ");
             scanf("%lf", &breakfastFee);
 
         }
 
-        //if arrival time is between lunch and breakfast ask for lunch and breakfast amount
         if(days == numDays && arrivalTime > 13.01 && arrivalTime <= 19.00)
         {
             printf("Enter the amount for breakfast: ");
@@ -76,7 +70,6 @@ void getMealAmount(int numberDays)
             scanf("%lf", &lunchFee);
         }
 
-        //ask for breakfast, lunch, and dinner if arriving past 7
         if(days == numDays && arrivalTime > 19.01)
         {
             printf("Enter the amount for breakfast: ");
@@ -88,15 +81,13 @@ void getMealAmount(int numberDays)
             printf("Enter the amount for dinner: ");
             scanf("%lf", &dinnerFee);
         }
-        
-        //Calculate total meal amount
+
         allowedBreakfastFee *= days;
         allowedLunchFee *= days;
         allowedDinnerFee *= days;
         double allowedMealtotal = allowedBreakfastFee + allowedLunchFee + allowedDinnerFee;
         double spentMealTotal = breakfastFee + lunchFee + dinnerFee;
-        
-        //return total meal amount
+
         printf("%f\n",allowedMealtotal);
         printf("%f", spentMealTotal);
     }
